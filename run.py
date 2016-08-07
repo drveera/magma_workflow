f404972dfe82238df3a328bfef93b72ec8652040
jsmp <jonatan.smp@gmail.com>
2016-08-07 15:05:13 +0100

    argparse adjustment

--- run.py
+++ run.py
@@ -1,9 +1,7 @@
 from subprocess import call
 import argparse
-from sys import argv
 import pickle
 
-parser = argparse.ArgumentParser()
 args = {'gwf': 'the path to the gwf workflow',
         'studyfolder': 'the folder containing the studies with the snp p values',
         'snploc': 'plink file stub, such as hapmap or 1000 genomes (see MAGMA documentation)',
@@ -11,12 +9,11 @@ args = {'gwf': 'the path to the gwf workflow',
         'setfile': 'gene set file (see MAGMA documentation)',
         'folder': 'folder where output files are placed'}
 
+parser = argparse.ArgumentParser()
 for arg, hlp in args.items():
     parser.add_argument('--' + arg, action='store', dest=arg, help=hlp)
-
-s = parser.parse_args(argv[1:])
+s = parser.parse_args()
 
 pickle.dump(s, open('.config.pl', 'wb'))
 
-call([s.gwf])
-
+call([s.gwf]):
